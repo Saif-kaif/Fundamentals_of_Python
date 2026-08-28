601ebb48f5e7ea6bbedeba5d0e858f5c9c95f3ae
Saif Kaif <machining1900@gmail.com>
2026-08-28 09:53:43 +0600

    initial commit

---
+++ first.py
@@ -0,0 +1,16 @@
+print("Hello World!!")
+
+print("")
+
+start=1
+end=5 
+while  start < end+1 :
+    print(start)
+    start+=1
+
+print("")
+
+start=6
+end=10
+for i in range(start,end):
+    print(i) 
